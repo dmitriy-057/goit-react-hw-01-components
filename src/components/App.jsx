fe31edc541e4e5729e7064be574446ca14aa7c5c
Dmitriy Gonharov <my.webpage057@gmail.com>
2022-09-11 02:41:19 +0300

    add 2st task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from "components/Profile/Profile";
+import { Statistics } from "components/Statistics/Statistics";
 import user from 'components/Profile/user.json'
+import data from 'components/Statistics/data.json'
 export const App = () => {
   return (
     <div>
@@ -9,6 +11,7 @@ export const App = () => {
       location={user.location}
       avatar={user.avatar}
       stats={user.stats}/>
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
